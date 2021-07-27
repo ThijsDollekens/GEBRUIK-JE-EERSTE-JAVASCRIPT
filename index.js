3ec187fa21214434cca0cb035b5cccdc0bfd6f0b
ThijsDollekens <87063168+ThijsDollekens@users.noreply.github.com>
2021-07-27 14:03:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello Winc Academy")
+
+let number = '1'
+console.log(number)
+
+let number1 = '9'
+console.log(number1)
+
+let age = 19
+console.log(typeof age)
+
+let age2 = '19'
+console.log(typeof age2)
+
+
